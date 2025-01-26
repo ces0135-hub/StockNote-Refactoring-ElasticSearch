9a56966773712cda3fb588de6d4271ea24d2c5ba
Sukyeong Sung <skyeong258@gmail.com>
2025-01-26 13:49:35 +0900

    Feat:  해시태그 SMS-38

--- src/main/java/org/com/stocknote/domain/post/controller/PostController.java
+++ src/main/java/org/com/stocknote/domain/post/controller/PostController.java
@@ -6,6 +6,7 @@ import org.com.stocknote.domain.post.dto.PostCreateDto;
 import org.com.stocknote.domain.post.dto.PostModifyDto;
 import org.com.stocknote.domain.post.dto.PostResponseDto;
 import org.com.stocknote.domain.post.service.PostService;
+import org.com.stocknote.global.dto.GlobalResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -13,42 +14,43 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/post")
+@RequestMapping("/api/v1/post")
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
     @PostMapping
-    public Long createPost(
+    public GlobalResponse<Long> createPost(
             @Valid @RequestBody PostCreateDto postCreateDto,
             Authentication authentication
     ) {
         String email = authentication.getName();
-        return postService.createPost(postCreateDto, email);
+        return GlobalResponse.success(postService.createPost(postCreateDto, email));
     }
 
     @GetMapping
-    public ResponseEntity<Page<PostResponseDto>> getAllPosts(Pageable pageable) {
-        return ResponseEntity.ok(postService.getPosts(pageable));
+    public GlobalResponse<Page<PostResponseDto>> getAllPosts(Pageable pageable) {
+        return GlobalResponse.success(postService.getPosts(pageable));
     }
 
     @GetMapping("/{id}")
-    public PostResponseDto getPostById(@PathVariable("id") Long id) {
-        return postService.getPostById(id);
+    public GlobalResponse<PostResponseDto> getPostById(@PathVariable("id") Long id) {
+        return GlobalResponse.success(postService.getPostById(id));
     }
 
     @PutMapping("/{id}")
-    public void updatePost(
+    public GlobalResponse<String> updatePost(
             @PathVariable("id") Long id,
             @Valid @RequestBody PostModifyDto postModifyDto
     ) {
         postService.updatePost(id, postModifyDto);
+        return GlobalResponse.success("Post updated successfully");
     }
 
     @DeleteMapping("/{id}")
-    public void deletePost(@PathVariable("id") Long id) {
-        // 게시글 삭제 권한 확인 필요 (작성자만 삭제 가능하다면)
+    public GlobalResponse<String> deletePost(@PathVariable("id") Long id) {
         postService.deletePost(id);
+        return GlobalResponse.success("Post deleted successfully");
     }
 }
--- src/main/java/org/com/stocknote/domain/post/service/PostService.java
+++ src/main/java/org/com/stocknote/domain/post/service/PostService.java
@@ -1,6 +1,8 @@
 package org.com.stocknote.domain.post.service;
 
 import lombok.RequiredArgsConstructor;
+import org.com.stocknote.domain.hashtag.entity.Hashtag;
+import org.com.stocknote.domain.hashtag.service.HashtagService;
 import org.com.stocknote.domain.member.entity.Member;
 import org.com.stocknote.domain.member.repository.MemberRepository;
 import org.com.stocknote.domain.post.dto.PostCreateDto;
@@ -13,32 +15,48 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
     private final MemberRepository memberRepository;
+    private final HashtagService hashtagService;
 
     @Transactional
     public Long createPost(PostCreateDto postCreateDto, String userEmail) {
         Member member = memberRepository.findByEmail(userEmail)
                 .orElseThrow(() -> new IllegalArgumentException("Member not found for email = " + userEmail));
+
         Post post = postCreateDto.toEntity(member.getId());
         Post savedPost = postRepository.save(post);
+
+        hashtagService.createHashtags(savedPost.getId(), postCreateDto.getHashtags());
         return savedPost.getId();
     }
 
     @Transactional(readOnly = true)
     public Page<PostResponseDto> getPosts(Pageable pageable) {
         Page<Post> posts = postRepository.findAll(pageable);
-        return posts.map(PostResponseDto::fromPost);
+        return posts.map(post -> {
+            List<String> hashtags = hashtagService.getHashtagsByPostId(post.getId()).stream()
+                    .map(Hashtag::getName).toList();
+            return PostResponseDto.fromPost(post, hashtags);
+        });
     }
 
     @Transactional(readOnly = true)
     public PostResponseDto getPostById(Long id) {
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Post not found"));
-        return PostResponseDto.fromPost(post);
+
+        List<String> hashtags = hashtagService.getHashtagsByPostId(post.getId())
+                .stream()
+                .map(Hashtag::getName)
+                .toList();
+
+        return PostResponseDto.fromPost(post, hashtags);
     }
 
     @Transactional
@@ -48,6 +66,8 @@ public class PostService {
         existingPost.bodyUpdate(postModifyDto.getBody());
         existingPost.titleUpdate(postModifyDto.getTitle());
         postRepository.save(existingPost);
+
+        hashtagService.updateHashtags(id, postModifyDto.getHashtags());
     }
 
     @Transactional
@@ -56,5 +76,7 @@ public class PostService {
                 .orElseThrow(() -> new RuntimeException("Post not found"));
         post.softDelete();
         postRepository.save(post);
+
+        hashtagService.deleteHashtagsByPostId(id);
     }
 }
