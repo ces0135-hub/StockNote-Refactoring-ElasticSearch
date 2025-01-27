f167c44b437aa1f977706312981b24fc409b5e3c
kknaks <benesia93@naver.com>
2025-01-27 17:18:57 +0900

    Fix: 스케쥴러수정(파일경로->클래스경로) SMS-73

--- src/test/java/org/com/stocknote/StockNoteApplicationTests.java
+++ src/test/java/org/com/stocknote/StockNoteApplicationTests.java
@@ -2,12 +2,15 @@ package org.com.stocknote;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application.yml")
 class StockNoteApplicationTests {
-
     @Test
-    void contextLoads () {
+    void contextLoads() {
+        // 기본 컨텍스트 로드 테스트
     }
-
 }
