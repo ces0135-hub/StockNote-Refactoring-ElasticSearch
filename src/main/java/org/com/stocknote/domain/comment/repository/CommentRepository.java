af0816c615b30488400644af376ef1be020ee507
Sukyeong Sung <skyeong258@gmail.com>
2025-02-12 11:16:29 +0900

    feat: 댓글 성능 보완

--- src/main/java/org/com/stocknote/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/com/stocknote/domain/comment/repository/CommentRepository.java
@@ -5,11 +5,18 @@ import org.com.stocknote.domain.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Page<Comment> findByPostId(Pageable pageable, Long postId);
+//    Page<Comment> findByPostId(Pageable pageable, Long postId);
     Page<Comment> findByMember(Member member, Pageable pageable);
     boolean existsByPostIdAndMember(Long postId, Member member);
+    @Query(value = "SELECT DISTINCT c FROM Comment c " +
+            "JOIN FETCH c.member " +
+            "WHERE c.post.id = :postId",
+            countQuery = "SELECT COUNT(c) FROM Comment c WHERE c.post.id = :postId")
+    Page<Comment> findByPostId(@Param("postId") Long postId, Pageable pageable);
 }
